9139bd495fa0c0b2d6e2492da9ccd002d2adc526
Stanislav Atanasov <lucifer@anavaro.com>
2015-08-06 02:03:53 +0300

    This should fix addresses for dynamicly loaded notifications

--- controller/main_controller.php
+++ controller/main_controller.php
@@ -24,6 +24,7 @@ class main_controller
 
 	public function base($last)
 	{
+		//$this->user->session_begin(false);
 		if ($this->user->data['user_id'] != ANONYMOUS && $this->user->data['is_registered'] == true && $this->user->data['is_bot'] == false)
 		{
 			$this->user->session_begin(false);
@@ -34,20 +35,23 @@ class main_controller
 		{
 			throw new \phpbb\exception\http_exception(403, 'NO_AUTH_OPERATION');
 		}
+
 		if ($this->request->is_ajax())
 		{
+			$this->user->session_begin(false);
 			return new \Symfony\Component\HttpFoundation\JsonResponse(array(
 				$response
 			));
 		}
 		else
 		{
-			//var_dump($response);
-			throw new \phpbb\exception\http_exception(403, 'NO_AUTH_OPERATION');
+			var_dump($response);
+			//throw new \phpbb\exception\http_exception(403, 'NO_AUTH_OPERATION');
 		}
 	}
 	protected function get_unread($last)
 	{
+		$this->user->session_begin(false);
 		$notifications_new = array();
 		$sql = 'SELECT notification_id FROM ' . NOTIFICATIONS_TABLE . '
 		WHERE notification_id > ' . $last . ' AND user_id = ' . $this->user->data['user_id'];
@@ -68,6 +72,26 @@ class main_controller
 			foreach ($notifications['notifications'] as $notification)
 			{
 				$tmp = $notification->prepare_for_display();
+				if($tmp['U_MARK_READ'] != '')
+				{
+					$mark = explode('?', $tmp['U_MARK_READ']);
+					$tmp['U_MARK_READ'] = $this->config['server_protocol'] . $this->config['server_name'] . '/index.php?' . $mark[1];
+				}
+				if ($tmp['URL'] != '')
+				{
+					$url = explode('/', $tmp['URL']);
+					if ($url[0] == '.')
+					{
+						foreach($url as $id => $el)
+						{
+							if ($el == '.' || $el == '..')
+							{
+								unset($url[$id]);
+							}
+						}
+						$tmp['URL'] = $this->config['server_protocol'] . $this->config['server_name'] . '/' . implode('/', $url);
+					}
+				}
 				$output['notifs'][] = $tmp;
 			}
 		}
