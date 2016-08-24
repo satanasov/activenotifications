5b4e055e0bd3e2d11a47f249bd8e89b864820e7b
kasimi <mail@kasimi.net>
2016-08-24 21:03:06 +0200

    Listener cleanup

--- event/listener.php
+++ event/listener.php
@@ -11,10 +11,6 @@
 
 namespace anavaro\activenotifications\event;
 
-/**
-* Event listener
-*/
-
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class listener implements EventSubscriberInterface
@@ -40,8 +36,8 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.acp_board_config_edit_add'	=>	'add_options',
-			'core.page_header'		=> 'setup',
+			'core.acp_board_config_edit_add'	=> 'add_options',
+			'core.page_header'					=> 'setup',
 		);
 	}
 
@@ -55,15 +51,13 @@ class listener implements EventSubscriberInterface
 	 * @param \phpbb\template\template		$template				Template object
 	 * @param \phpbb\notification\manager	$notification_manager	Notifications manager
 	 * @param \phpbb\controller\helper		$helper					Controller helper
-	 * @param \phpbb\request\request		$request				Request object
 	 */
 	public function __construct(
 		\phpbb\config\config				$config,
 		\phpbb\user							$user,
 		\phpbb\template\template			$template,
 		\phpbb\notification\manager			$notification_manager,
-		\phpbb\controller\helper			$helper,
-		\phpbb\request\request				$request
+		\phpbb\controller\helper			$helper
 	)
 	{
 		$this->config						= $config;
@@ -71,30 +65,30 @@ class listener implements EventSubscriberInterface
 		$this->template						= $template;
 		$this->notification_manager			= $notification_manager;
 		$this->helper						= $helper;
-		$this->request						= $request;
 	}
 
-	public function setup()
+	/**
+	 * object $event The event object
+	 */
+	public function setup($event)
 	{
 		if ($this->user->data['user_id'] != ANONYMOUS && $this->user->data['is_registered'] && !$this->user->data['is_bot'])
 		{
-			// Work with cookies
-			$cookie = $this->request->variable($this->config['cookie_name'] . '_an', '', true, \phpbb\request\request_interface::COOKIE);
-
 			$last = $this->get_last_notification();
-			$last = ($last) ? $last : 0;
 			$this->template->assign_vars(array(
-				'ACTIVE_NOTIFICATION_LAST'	=> $last,
-				'ACTIVE_NOTIFICATION_TIME'	=> $this->config['notification_pull_time'] * 1000,
-				'ACTIVE_NOTIFICATION_URL'	=> substr($this->helper->route('notifications_puller', array('last' => $last)), 0, strlen($last) * -1),
-				//'ACTIVE_NOTIFICATION_AVATAR_BASE'	=> 	$this->config['server_protocol'] . $this->config['server_name'] . '/download/file.php?avatar=',
+				'ACTIVE_NOTIFICATION_LAST'				=> $last,
+				'ACTIVE_NOTIFICATION_TIME'				=> $this->config['notification_pull_time'] * 1000,
+				'ACTIVE_NOTIFICATION_URL'				=> substr($this->helper->route('notifications_puller', array('last' => $last)), 0, -strlen($last)),
 				'ACTIVE_NOTIFICATIONS_COOKIE_DOMAIN'	=> $this->config['cookie_domain'],
-				'ACTIVE_NOTIFICATIONS_COOKIE_NAME'	=> $this->config['cookie_name'] . '_an',
-				'ACTIVE_NOTIFICATIONS_COOKIE_PATH'	=> $this->config['cookie_path'],
+				'ACTIVE_NOTIFICATIONS_COOKIE_NAME'		=> $this->config['cookie_name'] . '_an',
+				'ACTIVE_NOTIFICATIONS_COOKIE_PATH'		=> $this->config['cookie_path'],
 			));
 		}
 	}
 
+	/**
+	 * @param object $event The event object
+	 */
 	public function add_options($event)
 	{
 		if ($event['mode'] == 'features')
@@ -102,11 +96,13 @@ class listener implements EventSubscriberInterface
 			// Store display_vars event in a local variable
 			$display_vars = $event['display_vars'];
 			$my_config_vars = array(
-				'legend10'	=> 'ACTIVE_NOTIFICATIONS',
+				'legend10'					=> 'ACTIVE_NOTIFICATIONS',
 				'notification_pull_time'	=> array('lang' => 'ACTIVE_NOTIFICATIONS_TIME', 'validate' => 'int:0:99', 'type' => 'number', 'explain' => true),
 			);
+
 			// Insert my config vars after...
 			$insert_after = 'LOAD_CPF_VIEWTOPIC';
+
 			// Rebuild new config var array
 			$position = array_search($insert_after, array_keys($display_vars['vars'])) - 1;
 			$display_vars['vars'] = array_merge(
@@ -114,24 +110,23 @@ class listener implements EventSubscriberInterface
 				$my_config_vars,
 				array_slice($display_vars['vars'], $position)
 			);
+
 			$event['display_vars'] = array('title' => $display_vars['title'], 'vars' => $display_vars['vars']);
 		}
 	}
 
+	/**
+	 * @return int
+	 */
 	protected function get_last_notification()
 	{
 		$last_notification = $this->notification_manager->load_notifications(array('limit' => 1));
+
 		foreach ($last_notification['notifications'] as $notification)
 		{
-			$notifs = $notification->prepare_for_display();
+			return (int) $notification->notification_id;
 		}
-		if (isset($notifs))
-		{
-			return (int) $notifs['NOTIFICATION_ID'];
-		}
-		else
-		{
-			return 0;
-		}
+
+		return 0;
 	}
 }
